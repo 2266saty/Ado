131c599e49da5b98fe8b81f252fe6a3216c06e12
2266saty <2266satyajit22@gmail.com>
2022-05-16 16:55:35 +0530

    ado

---
+++ FootballLeagueDBApproach/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FootballLeague2;
+
+
+
+namespace FootballLeague2.Controllers
+{
+    public class HomeController : Controller
+    {
+        mydatabaseEntities1 context = new mydatabaseEntities1();
+        public ActionResult Index()
+        {
+            return View(context.FootballLeagues);
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+
+
+            return View();
+        }
+
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+
+
+            return View();
+        }
+    }
+}
